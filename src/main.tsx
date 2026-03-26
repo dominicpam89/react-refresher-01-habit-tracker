309a62a41f5ac355454e34cd8faf1d0d334c2590
dominicpam89 <dominicpam89@gmail.com>
2026-03-26 15:46:35 +0700

    establish all ContextProviders in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ThemeContextProvider from './context/ThemeContextProvider.tsx';
+import HabitContextProvider from './context/HabitContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+   <StrictMode>
+      <ThemeContextProvider>
+         <HabitContextProvider>
+            <App />
+         </HabitContextProvider>
+      </ThemeContextProvider>
+   </StrictMode>
 );
